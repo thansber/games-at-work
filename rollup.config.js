f1a61932f0a3ca0f8091ee96d3339aaece55f6e0
thansber <thansber@yahoo.com>
2019-12-06 15:36:04 -0500

    add firebase deploy and copy assets for build

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,20 @@
 import { createDefaultConfig } from '@open-wc/building-rollup';
+import copy from 'rollup-plugin-copy-assets';
 
 // if you need to support IE11 use "modern-and-legacy-config" instead.
 // import { createCompatibilityConfig } from '@open-wc/building-rollup';
 // export default createCompatibilityConfig({ input: './index.html' });
 
-export default createDefaultConfig({ input: './src/index.html' });
+const config = createDefaultConfig({
+  input: './src/index.html'
+});
+
+export default {
+  ...config,
+  plugins: [
+    ...config.plugins,
+    copy({
+      assets: ['./src/fonts', './src/images']
+    })
+  ]
+};
